c5eb32bed0d1725277e9cd4b060043a9046cbe6e
Reda-bx <reeda.bx@gmail.com>
2016-08-23 21:37:06 +0100

    es5

--- app.js
+++ app.js
@@ -9,11 +9,11 @@ app.get('/', (req, res) => {
   fetch(url)
   .then(response => response.json())
   .then(body => {
-    let items = body.items
-    let _items = items[0]
-    let arr = Object.keys(_items).map(function (key) {return {name: key, time: _items[key]}})
-    let timeNow = moment().format()
-    let soon = {}
+    var items = body.items
+    var _items = items[0]
+    var arr = Object.keys(_items).map(function (key) {return {name: key, time: _items[key]}})
+    var timeNow = moment().format()
+    var soon = {}
     arr.forEach((value, index) => {
       if(moment(timeNow, 'h:mm a').isBetween(moment(value.time, 'h:mm a'), moment(arr[index+1].time, 'h:mm a'))){
         res.json({text: arr[index+1].name + 'at: ' + arr[index+1].time})
