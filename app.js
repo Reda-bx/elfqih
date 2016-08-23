d121856f747e3e439418392f65563e2b7ca7c22f
Reda-bx <reeda.bx@gmail.com>
2016-08-23 21:42:33 +0100

    commit

--- app.js
+++ app.js
@@ -1,26 +1,25 @@
-'use strict'
-
-const express = require('express')
-const app = express()
-const fetch = require('node-fetch')
-const moment = require('moment')
-const KEY = 'a95015e8416df0519c21af85a9cf4e02'
-const url = `http://muslimsalat.com/daily.json?key=${KEY}`
+var express = require('express')
+var app = express()
+var fetch = require('node-fetch')
+var moment = require('moment')
+var KEY = 'a95015e8416df0519c21af85a9cf4e02'
+var url = `http://muslimsalat.com/daily.json?key=${KEY}`
 
 app.get('/', (req, res) => {
   fetch(url)
   .then(response => response.json())
   .then(body => {
-    let items = body.items
-    let _items = items[0]
-    let arr = Object.keys(_items).map(function (key) {return {name: key, time: _items[key]}})
-    let timeNow = moment().format()
-    let soon = {}
+    var items = body.items
+    var _items = items[0]
+    var arr = Object.keys(_items).map(function (key) {return {name: key, time: _items[key]}})
+    var timeNow = moment().format()
+    var soon = {}
     arr.forEach((value, index) => {
       if(moment(timeNow, 'h:mm a').isBetween(moment(value.time, 'h:mm a'), moment(arr[index+1].time, 'h:mm a'))){
         res.json({text: arr[index+1].name + 'at: ' + arr[index+1].time})
       }
     })
   })
+  .catch(err => res.json({error: err}))
 })
 app.listen(3000, () => console.log('runinig...'))
