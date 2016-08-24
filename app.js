a9129d6c3f207fe6045a0ce264b6e199ddbe47ca
Reda-bx <reeda.bx@gmail.com>
2016-08-24 08:48:59 +0100

    rabat time

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var fetch = require('node-fetch')
 var moment = require('moment')
 
 var KEY = 'a95015e8416df0519c21af85a9cf4e02'
-var url = 'http://muslimsalat.com/daily.json?key='+KEY
+var url = 'http://muslimsalat.com/rabat/daily.json?key='+KEY
 
 app.use(bodyParser.json())
 
@@ -23,7 +23,7 @@ app.post('/', (req, res) => {
     var soon = arr_of_items.filter((value) => value.time > timeNow)
     var _soon = soon[0]
     var text = 'Hi , Salat *' + _soon.name + '* in *'+ moment(timeNow, 'HH:mm').to(moment(soon[0].time, 'HH:mm'), true) + '*. Adan at: *' + _soon.time +'*'
-    res.send({text: moment().format('HH:mm')})
+    res.send({text: text})
   })
   .catch(err => res.json({error: err}))
 })
