22115476e8b6c02faedccd87892115770dd04cd0
Reda-bx <reeda.bx@gmail.com>
2016-08-24 09:19:06 +0100

    sqd

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 var bodyParser = require('body-parser')
 var fetch = require('node-fetch')
-var moment = require('moment')
+var moment = require('moment-timezone')
 
 var KEY = 'a95015e8416df0519c21af85a9cf4e02'
 var url = 'http://muslimsalat.com/rabat/daily.json?key='+KEY
@@ -19,7 +19,7 @@ app.post('/', (req, res) => {
     var arr_of_items = Object.keys(_items).map(function (key) {return {name: key, time: _items[key]}})
     arr_of_items.shift()
     arr_of_items.map(value => value.time = moment(value.time, 'h:mm a').format('HH:mm'))
-    var timeNow = moment().local().format('HH:mm')
+    var timeNow = moment().tz(moment.tz.guess()).format('HH:mm')
     var soon = arr_of_items.filter((value) => value.time > timeNow)
     var _soon = soon[0]
     var text = 'Hi , Salat *' + _soon.name + '* in *'+ moment(timeNow, 'HH:mm').to(moment(soon[0].time, 'HH:mm'), true) + '*. Adan at: *' + _soon.time +'*'
