8f4c8243917af91c97e238b004d8a68e269d5b46
Reda-bx <reeda.bx@gmail.com>
2016-08-24 07:50:05 +0100

    Refactor bb

--- app.js
+++ app.js
@@ -3,30 +3,22 @@ var app = express()
 var fetch = require('node-fetch')
 var moment = require('moment')
 var KEY = 'a95015e8416df0519c21af85a9cf4e02'
-var url = `http://muslimsalat.com/daily.json?key=${KEY}`
+var url = 'http://muslimsalat.com/daily.json?key='+KEY
 
 app.post('/', (req, res) => {
   fetch(url)
   .then(response => response.json())
   .then(body => {
-    var items = body.items
-    var _items = items[0]
-    var arr = Object.keys(_items).map(function (key) {return {name: key, time: _items[key]}})
-    var timeNow = moment().format()
-    var text = ''
-    arr.forEach((value, index) => {
-      if(index === 6) {
-        if(text === '') {
-          res.json({text: text})
-        }else{
-          text = 'Im Sleeping, come back at 00:00'
-          res.json({text: text})
-        }
-      }
-      if(moment(timeNow, 'h:mm a').isBetween(moment(value.time, 'h:mm a'), moment(arr[index+1].time, 'h:mm a'))){
-        text = arr[index+1].name + 'at: ' + arr[index+1].time
-      }
-    })
+    // get items
+    var _items = body.items[0]
+    var arr_of_items = Object.keys(_items).map(function (key) {return {name: key, time: _items[key]}})
+    arr_of_items.shift()
+    arr_of_items.map(value => value.time = moment(value.time, 'h:mm a').format('HH:mm'))
+    var timeNow = moment().format('HH:mm')
+    var soon = arr_of_items.filter((value) => value.time > timeNow)
+    var _soon = soon[0]
+    var text = 'Hi, Salat ' + _soon.name + ' in '+ moment(timeNow, 'HH:mm').to(moment(soon[0].time, 'HH:mm'), true) + '. Adan at: ' + _soon.time
+    res.send({text: text})
   })
   .catch(err => res.json({error: err}))
 })
